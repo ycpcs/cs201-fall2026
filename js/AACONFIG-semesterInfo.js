0f3e5df74d724873e5d9963dfe3d4307520063a9
dchen3-ycp <dchen3@ycp.edu>
2026-08-03 19:10:07 -0400

    Update AACONFIG-semesterInfo.js

--- js/AACONFIG-semesterInfo.js
+++ js/AACONFIG-semesterInfo.js
@@ -2,14 +2,15 @@
 var semesterInfo = {
     // The dates for the first and last day of classes (not including finals week).
     // Set the time to 4 AM ... because why not.
-    firstDayOfSemester: new Date("01/27/2022 4:00:00"),
-    lastDayOfSemester:  new Date("05/06/2022 4:00:00"),
+    firstDayOfSemester: new Date("08/24/2026 4:00:00"),
+    lastDayOfSemester:  new Date("12/07/2026 4:00:00"),
 
     // Specify days on which there are no classes held at the college.
     // Each break is specified by a startDate and an endDate.
     // For single-day breaks, the startDate and endDate are the same.
     vacationDates: [
-        new VacationDays("Winter Break",          new Date("03/10/2022"),  new Date("03/13/2022")),
-        new VacationDays("Spring Break",         new Date("04/15/2022"), new Date("04/18/2022")),
+        new VacationDays("Labor Day", new Date("09/07/2026"), new Date("09/07/2026")),
+        new VacationDays("Fall Break", new Date("10/09/2026"), new Date("10/14/2026")),
+        new VacationDays("Thanksgiving Break", new Date("11/25/2026"), new Date("11/30/2026")),
     ]
 };
