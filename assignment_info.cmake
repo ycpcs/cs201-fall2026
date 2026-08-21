fb0c231353f4a0dd367e5f6fde677827fc9028d8
dchen3-ycp <dchen3@ycp.edu>
2026-08-21 15:17:40 -0400

    Add files via upload

---
+++ assignment_info.cmake
@@ -0,0 +1,8 @@
+
+############################################################
+# Assignment specific info
+############################################################
+set(COURSE_NAME "CS 201")
+set(TERM "Fall")
+set(PROJECT_NUMBER "lab01")
+#set(PROJECT_NAME IntArrayStack)
